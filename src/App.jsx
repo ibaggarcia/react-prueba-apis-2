ce06475f8a1e89ec8d5565017d5eaeb103a57bb6
Gabriel Garcia <gabrielgarcia.udg@gmail.com>
2023-09-12 17:46:46 -0600

    Final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [catFact, setCatFact] = useState("");
+  const [cat, setCat] = useState(null);
+  const [newCat, setNewCat] = useState(false);
+
+  useEffect(() => {
+    let shortFact = "";
+
+    fetch("https://catfact.ninja/fact")
+      .then((res) => res.json())
+      .then((data) => {
+        setCatFact(data.fact || "Gato");
+
+        shortFact = data.fact.split(" ").slice(0,1).toString()
+        setCat(shortFact)
+      });
+  },[newCat]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={`https://cataas.com/cat/says/${cat}`} alt="" />
+      <h1>{catFact}</h1>
+      <button onClick={() => setNewCat(!newCat)}>Nuevo dato</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
